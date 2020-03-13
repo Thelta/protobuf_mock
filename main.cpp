feedbf7d2f4e0e3dd9da871c900be6d9dbc3d146
Atılhan Emre Dursunoğlu <atilhandursunoglu@outlook.com>
2020-03-13 18:44:23 +0300

    I didn't see argument when copy and pasted.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main()
 {
     google::protobuf::Arena a;
     foo::Foo *array = google::protobuf::Arena::CreateArray<foo::Foo>(&a, 10);
-    foo::Foo *message = google::protobuf::Arena::CreateMessage<foo::Foo>(&a, 10);
+    foo::Foo *foo = google::protobuf::Arena::CreateMessage<foo::Foo>(&a);
     return 0;
 }
